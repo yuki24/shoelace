60b007d52a362854343bdd6fb2192faf656f49fc
Cory LaViska <cory@abeautifulsite.net>
2021-03-31 12:04:01 -0400

    Add test helper

--- tests/helpers.js
+++ tests/helpers.js
@@ -3,15 +3,13 @@ import { fixture, html, nextFrame } from '@open-wc/testing';
 // We wrap `fixture()` and wait a couple cycles until the fixture has been painted. This prevents us from having to
 // include multiple calls to `nextFrame()` in every test.
 export async function createFixture(markup) {
-  return new Promise(async resolve => {
-    const el = fixture(html`${markup}`);
+  const el = await fixture(html`${markup}`);
 
-    // await DOM to page
-    await nextFrame();
+  // await DOM to page
+  await nextFrame();
 
-    // await style calc + painting
-    await nextFrame();
+  // await style calc + painting
+  await nextFrame();
 
-    resolve(el);
-  });
+  return el;
 }
